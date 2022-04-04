b744f6c261152320c58512cde2e76038c5dfeac1
Ambika <amika@aptonworks.com>
2022-04-04 16:39:33 +0530

    sidebar

---
+++ src/components/Sidebar.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import * as RiIcons from 'react-icons/ri'; 
+import * as IoIcons from 'react-icons/io';
+
+export const sidebarData=[
+    {
+        title:'Home',
+        path:'/',
+        icon:<AiIcons.AiFillHome />,
+        className:'nav-text'
+    },
+    {
+        title:'Products',
+        path:'/products',
+        icon:<FaIcons.FaProductHunt />,
+        className:'nav-text'
+    },
+    {
+        title:'Services',
+        path:'/Services',
+        icon:<RiIcons.RiServiceLine />,
+        className:'nav-text'
+    },
+    {
+        title:'Reports',
+        path:'/Reports',
+        icon:<IoIcons.IoIosPaper />,
+        className:'nav-text'
+    },
+    {
+        title:'Message',
+        path:'/Message',
+        icon:<FaIcons.FaEnvelopeOpenText />,
+        className:'nav-text'
+    },
+    {
+        title:'Support',
+        path:'/Support',
+        icon:<IoIcons.IoMdHelpCircle />,
+        className:'nav-text'
+    },
+]
